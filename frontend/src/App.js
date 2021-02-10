2daef4a9c45ff32f9a8e45d20284ffa86ad402c3
Saurabh Sen <saurabhsensaurabh@gmail.com>
2021-02-10 18:38:47 +0530

    create basic frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import HomeScreen from './Components/screens/HomeScreen';
+import ProductScreen from './Components/screens/ProductScreen';
 
 function App() {
   return (
-    <div className="app">
-
-    </div>
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomeScreen}/>
+          <Route path="/product/:id" component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
