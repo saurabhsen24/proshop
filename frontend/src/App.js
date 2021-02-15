d5d63fa577ffb1ebfcdcb820de6d3cad4f08408d
Saurabh Sen <saurabhsensaurabh@gmail.com>
2021-02-15 21:03:14 +0530

    order feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,14 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import CartScreen from './Components/screens/CartScreen';
 import HomeScreen from './Components/screens/HomeScreen';
+import LoginScreen from './Components/screens/LoginScreen';
 import ProductScreen from './Components/screens/ProductScreen';
+import ProfileScreen from './Components/screens/ProfileScreen';
+import RegisterScreen from './Components/screens/RegisterScreen';
+import ShippingScreen from './Components/screens/ShippingScreen';
+import PaymentScreen from './Components/screens/PaymentScreen';
+import PlaceOrderScreen from './Components/screens/PlaceOrderScreen';
+import OrderScreen from './Components/screens/OrderScreen';
 
 function App() {
   return (
@@ -13,9 +20,16 @@ function App() {
       <Header/>
       <main className="py-3">
         <Container>
-          <Route exact path="/" component={HomeScreen}/>
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/login' component={LoginScreen} />
           <Route path="/product/:id" component={ProductScreen}/>
           <Route path="/cart/:id?" component={CartScreen}/>
+          <Route exact path="/" component={HomeScreen}/>
         </Container>
       </main>
       <Footer/>
